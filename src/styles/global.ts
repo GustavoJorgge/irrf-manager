f38a7062a71512358da3bdef4f11fd7543312cea
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-12-10 01:23:12 -0300

    fix: antialiesed

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,22 +2,23 @@ import {createGlobalStyle} from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
 
-
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-
 }
 
-    body{
-        background-color: ${props => props.theme['gray-100']};
-        color: ${props => props.theme.black};
-        -webkit-font-smoothing: antialiased;
-    }
+body{
+    background-color: ${props => props.theme['gray-100']};
+    color: ${props => props.theme.black};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
 
-    body, input, textarea, button{
-        font: 400 1rem Roboto, sans-serif;
-    }
+body, input, textarea, button{
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+}
 
 `
